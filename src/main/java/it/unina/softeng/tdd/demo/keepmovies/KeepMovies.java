09148e98e734af4a0c317b8d501c9527fb1fdaaa
PZM7 <pedrozaho7@gmail.com>
2023-05-09 18:15:56 +0200

    As a user, I want to save movies so I won't forget about them.

--- src/main/java/it/unina/softeng/tdd/demo/keepmovies/KeepMovies.java
+++ src/main/java/it/unina/softeng/tdd/demo/keepmovies/KeepMovies.java
@@ -1,5 +1,20 @@
 package it.unina.softeng.tdd.demo.keepmovies;
 
-public class KeepMovies {
+import java.util.ArrayList;
+import java.util.List;
 
+public class KeepMovies {
+    private List<Movie> movies;
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public KeepMovies() {
+        movies = new ArrayList<>();
+    }
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
 }
--- src/test/java/it/unina/softeng/tdd/demo/keepmovies/KeepMoviesSpecification.java
+++ src/test/java/it/unina/softeng/tdd/demo/keepmovies/KeepMoviesSpecification.java
@@ -3,6 +3,7 @@ package it.unina.softeng.tdd.demo.keepmovies;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Year;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 class KeepMoviesSpecification {
-	
-	void someMoviesInCaseYouNeedThem() {
-		Movie joker, jojo, dunkirk, up;
+
+	Movie joker, jojo, dunkirk, up;
+
+	@BeforeEach
+	void init() {
 		joker   = new Movie("Joker",Year.of(2019),"thriller");
 		jojo    = new Movie("Jojo Rabbit", Year.of(2019), "comedy-drama");
 		dunkirk = new Movie("Dunkirk",Year.of(2017),"war");
 		up      = new Movie("Up",Year.of(2009),"comedy-drama");
+	}
+
+	@Test
+	public void testAddMovie() {
+		KeepMovies keepMovies = new KeepMovies();
+		keepMovies.addMovie(joker);
+		keepMovies.addMovie(jojo);
+		List<Movie> movies = keepMovies.getMovies();
+		assertEquals(2, movies.size());
+		assertTrue(movies.contains(joker));
+		assertTrue(movies.contains(jojo));
 	}
 	
 }
